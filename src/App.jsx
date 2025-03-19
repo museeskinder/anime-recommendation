09a8e8f98aa9357b9d0cfe2c9281554ca703c4c4
museeskinder <eskindermuse@gmail.com>
2025-03-19 14:26:47 +0300

    feat: update on anme fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { AnimeView } from "./assets/components/AnimeView/AnimeView";
+import { AnimeHome } from "./assets/components/AnimeHome/AnimeHome";
 import { AnimeTrending } from "./assets/components/AnimeTrending/AnimeTrending";
 import { useEffect, useState } from "react";
 import { AnimeAPI } from "./api/anime";
 export const App = () => {
   const [randomAnime, setRandomAnime] = useState();
 
-  const getRandomAnime = () => {
-    let randomNum = Math.floor(Math.random() * 10);
-    const anime = AnimeAPI.fetchAnime(randomNum);
+  const getRandomAnime = async () => {
+    let randomNum = Math.floor(Math.random() * 10) + 1;
+    const anime = await AnimeAPI.fetchAnime(randomNum);
     setRandomAnime(anime);
   };
 
   useEffect(() => {
     getRandomAnime();
-  });
+  }, []);
 
   return (
     <div>
-      <AnimeView anime={randomAnime}/>
+      <AnimeHome anime={randomAnime}/>
       <AnimeTrending />
     </div>
   );
--- src/api/anime.js
+++ src/api/anime.js
@@ -1,11 +1,12 @@
-import { axios } from 'axios';
+import  axios  from 'axios';
 import { BASE_URL } from './api-config';
 
 export class AnimeAPI {
     static async fetchAnime (id) {
-        const response = await axios.get('BASE_URL' + '/anime' + `/${id}`);
+        const response = await axios.get(`${BASE_URL}` + '/anime' + `/${id}`);
         return response.data;
     }
 }
+
 
 
