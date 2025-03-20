696fbc47f80db9d20ab8bb5fa77cc7fc0bc2a085
museeskinder <eskindermuse@gmail.com>
2025-03-20 15:38:43 +0300

    feat: added trending anime fetching

--- src/api/anime.js
+++ src/api/anime.js
@@ -1,8 +1,14 @@
 import  axios  from 'axios';
 import { BASE_URL } from './api-config';
+import { TRENDING_ANIME_URL } from './api-config';
 
     export const fetchAnime = async (id) => {
         const response = await axios.get(`${BASE_URL}` + '/anime' + `/${id}`);
+        return response.data;
+    }
+
+    export const fetchTrendingAnime = async () => {
+        const response = await axios.get(TRENDING_ANIME_URL);
         return response.data;
     }
 
